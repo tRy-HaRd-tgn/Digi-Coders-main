a938b02b2db9fb00c2f7d8598829997b3ebfe228
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-07-14 20:43:20 +0300

    feat: update chapter settings

--- frontend/src/components/blockly/getPythonToolbox.js
+++ frontend/src/components/blockly/getPythonToolbox.js
@@ -14,114 +14,20 @@ export const getPythonToolbox = () => {
         name: "Lists",
         colour: "#745CA6",
         contents: [
+          { kind: "block", blockxml: '<block type="lists_indexOf"></block>' },
           {
             kind: "block",
-            blockxml:
-              '    <block type="lists_indexOf">\n' +
-              '      <field name="END">FIRST</field>\n' +
-              '      <value name="VALUE">\n' +
-              '        <block type="variables_get">\n' +
-              '          <field name="VAR" id="e`(L;x,.j[[XN`F33Q5." variabletype="">list</field>\n' +
-              "        </block>\n" +
-              "      </value>\n" +
-              "    </block>\n",
+            blockxml: '<block type="lists_create_with"></block>',
           },
+          { kind: "block", blockxml: '<block type="lists_repeat"></block>' },
+          { kind: "block", blockxml: '<block type="lists_getIndex"></block>' },
+          { kind: "block", blockxml: '<block type="lists_setIndex"></block>' },
           {
             kind: "block",
-            blockxml:
-              '    <block type="lists_create_with">\n' +
-              '      <mutation items="0"></mutation>\n' +
-              "    </block>\n",
+            blockxml: '<block type="lists_getSublist"></block>',
           },
-          {
-            kind: "block",
-            blockxml:
-              '    <block type="lists_repeat">\n' +
-              '      <value name="NUM">\n' +
-              '        <shadow type="math_number">\n' +
-              '          <field name="NUM">5</field>\n' +
-              "        </shadow>\n" +
-              "      </value>\n" +
-              "    </block>\n",
-          },
-          {
-            kind: "block",
-            type: "lists_length",
-          },
-          {
-            kind: "block",
-            type: "lists_isEmpty",
-          },
-          {
-            kind: "block",
-            blockxml:
-              '    <block type="lists_create_with">\n' +
-              '      <mutation items="3"></mutation>\n' +
-              "    </block>\n",
-          },
-          {
-            kind: "block",
-            blockxml:
-              '    <block type="lists_getIndex">\n' +
-              '      <mutation statement="false" at="true"></mutation>\n' +
-              '      <field name="MODE">GET</field>\n' +
-              '      <field name="WHERE">FROM_START</field>\n' +
-              '      <value name="VALUE">\n' +
-              '        <block type="variables_get">\n' +
-              '          <field name="VAR" id="e`(L;x,.j[[XN`F33Q5." variabletype="">list</field>\n' +
-              "        </block>\n" +
-              "      </value>\n" +
-              "    </block>\n",
-          },
-          {
-            kind: "block",
-            blockxml:
-              '    <block type="lists_setIndex">\n' +
-              '      <mutation at="true"></mutation>\n' +
-              '      <field name="MODE">SET</field>\n' +
-              '      <field name="WHERE">FROM_START</field>\n' +
-              '      <value name="LIST">\n' +
-              '        <block type="variables_get">\n' +
-              '          <field name="VAR" id="e`(L;x,.j[[XN`F33Q5." variabletype="">list</field>\n' +
-              "        </block>\n" +
-              "      </value>\n" +
-              "    </block>\n",
-          },
-          {
-            kind: "block",
-            blockxml:
-              '    <block type="lists_getSublist">\n' +
-              '      <mutation at1="true" at2="true"></mutation>\n' +
-              '      <field name="WHERE1">FROM_START</field>\n' +
-              '      <field name="WHERE2">FROM_START</field>\n' +
-              '      <value name="LIST">\n' +
-              '        <block type="variables_get">\n' +
-              '          <field name="VAR" id="e`(L;x,.j[[XN`F33Q5." variabletype="">list</field>\n' +
-              "        </block>\n" +
-              "      </value>\n" +
-              "    </block>\n",
-          },
-          {
-            kind: "block",
-            blockxml:
-              '    <block type="lists_split">\n' +
-              '      <mutation mode="SPLIT"></mutation>\n' +
-              '      <field name="MODE">SPLIT</field>\n' +
-              '      <value name="DELIM">\n' +
-              '        <shadow type="text">\n' +
-              '          <field name="TEXT">,</field>\n' +
-              "        </shadow>\n" +
-              "      </value>\n" +
-              "    </block>\n",
-          },
-          {
-            kind: "block",
-            blockxml:
-              '    <block type="lists_sort">\n' +
-              '      <field name="TYPE">NUMERIC</field>\n' +
-              '      <field name="DIRECTION">1</field>\n' +
-              "    </block>\n",
-          },
+          { kind: "block", blockxml: '<block type="lists_split"></block>' },
+          { kind: "block", blockxml: '<block type="lists_sort"></block>' },
         ],
       },
       {
@@ -129,37 +35,9 @@ export const getPythonToolbox = () => {
         name: "Logic",
         colour: 210,
         contents: [
-          {
-            kind: "block",
-            type: "controls_if",
-          },
-          {
-            kind: "block",
-            blockxml:
-              '<block type="logic_compare"><field name="OP">EQ</field></block>',
-          },
-          {
-            kind: "block",
-            blockxml:
-              '<block type="logic_operation"><field name="OP">AND</field></block>',
-          },
-          {
-            kind: "block",
-            type: "logic_negate",
-          },
-          {
-            kind: "block",
-            blockxml:
-              '<block type="logic_boolean"><field name="BOOL">TRUE</field></block>',
-          },
-          {
-            kind: "block",
-            type: "logic_null",
-          },
-          {
-            kind: "block",
-            type: "logic_ternary",
-          },
+          { kind: "block", blockxml: '<block type="logic_compare"></block>' },
+          { kind: "block", blockxml: '<block type="logic_operation"></block>' },
+          { kind: "block", blockxml: '<block type="logic_boolean"></block>' },
         ],
       },
       {
@@ -371,7 +249,7 @@ export const getPythonToolbox = () => {
           },
           {
             kind: "block",
-            type: "math_random_float",
+            blockxml: '<block type="math_random_float"></block>',
           },
         ],
       },
